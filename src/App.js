dd97ec187745be53328571774a043607226fe2ab
Blaurt <inflamory@gmail.com>
2020-11-14 03:41:11 +0200

    add version 4

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>AWS test</p>
-        <p>v4</p>
+        <p>AWS test v4</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
